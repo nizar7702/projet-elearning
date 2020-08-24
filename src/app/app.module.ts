430e206764d4c5995de0b39e1daad396c21d0802
nizar2077 <nizar.bousabat2000@gmail.com>
2020-08-24 15:22:46 +0100

    partie1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/public/login/login.component';
+import { RegisterComponent } from './components/public/register/register.component';
+import { HomeComponent } from './components/public/home/home.component';
+import { NavbarComponent } from './components/public/navbar/navbar.component';
+import { FooterComponent } from './components/public/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
